b24745353823706569ff00a429820242bfbb084a
Rhys_M <rhysm1988@gmail.com>
2021-08-06 23:38:38 +0100

    moved app image to releases

--- electron/main.js
+++ electron/main.js
@@ -1,60 +1,81 @@
 const path = require('path');
 const url = require('url');
-const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron');
-const { channels } = require('../src/shared/constants');
+const {app, BrowserWindow, ipcMain, ipcRenderer, globalShortcut} = require('electron');
+const {channels} = require('../src/shared/constants');
 const ws = require('./ws')
 ws.ws()
 const Carplay = require('node-carplay')
 
 
 let mainWindow;
-function createWindow () {
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../index.html'),
-    protocol: 'file:',
-    slashes: true,
-  });
 
-  globalShortcut.register('f5', function() {
-    console.log('f5 is pressed')
-    mainWindow.webContents.openDevTools()
-})
+function createWindow() {
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '../index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
 
-  mainWindow = new BrowserWindow({ width: 800, height: 600, kiosk: true, webPreferences : {
-	preload: path.join(__dirname, 'preload.js'),
-	contextIsolation: false
-}});
-  mainWindow.loadURL(startUrl);
-  let size = mainWindow.getSize()
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-  const config = {
-      dpi: 240,
-      nightMode: 0,
-      hand: 0,
-      boxName: 'nodePlay',
-      width: size[0],
-      height: size[1],
-      fps: 30,
-  }
-  console.log("spawning carplay", config)
-  const carplay = new Carplay(config)
-  ipcMain.on('click', (event, data) => {
-    	  carplay.sendTouch(data.type, data.x, data.y)
-	  console.log(data.type, data.x, data.y)
-  })
+    globalShortcut.register('f5', function () {
+        console.log('f5 is pressed')
+        mainWindow.webContents.openDevTools()
+    })
+
+    mainWindow = new BrowserWindow({
+        width: 800, height: 600, kiosk: true, webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: false
+        }
+    });
+    mainWindow.loadURL(startUrl);
+    let size = mainWindow.getSize()
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+    const config = {
+        dpi: 240,
+        nightMode: 0,
+        hand: 0,
+        boxName: 'nodePlay',
+        width: size[0],
+        height: size[1],
+        fps: 30,
+    }
+    console.log("spawning carplay", config)
+    const carplay = new Carplay(config)
+    carplay.on('status', (data) => {
+        if(data.status) {
+            mainWindow.webContents.send('plugged')
+        } else {
+            mainWindow.webContents.send('unplugged')
+        }
+        console.log("data received", data)
+
+    })
+    ipcMain.on('click', (event, data) => {
+        carplay.sendTouch(data.type, data.x, data.y)
+        console.log(data.type, data.x, data.y)
+    })
+    ipcMain.on('statusReq', (event, data) => {
+        if(carplay.getStatus()) {
+            mainWindow.webContents.send('plugged')
+        } else {
+            mainWindow.webContents.send('unplugged')
+        }
+    })
 }
+
 app.on('ready', createWindow);
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
+
 
 
--- src/react/App.js
+++ src/react/App.js
@@ -1,118 +1,143 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "@fontsource/montserrat";
 import JsmpegPlayer from "./JsmpegPlayer";
-const { ipcRenderer } = window;
+
+const {ipcRenderer} = window;
 
 
 class App extends Component {
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			height: 0,
-			width: 0,
-			mouseDown: false,
-			lastX: 0,
-			lastY: 0
-		}
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            height: 0,
+            width: 0,
+            mouseDown: false,
+            lastX: 0,
+            lastY: 0,
+            status: false
+        }
+    }
 
-	componentDidMount() {
-		const height = this.divElement.clientHeight
-		const width = this.divElement.clientWidth
-		this.setState({height, width}, () => {console.log(this.state.height, this.state.width)})
-	}
+    componentDidMount() {
+        const height = this.divElement.clientHeight
+        const width = this.divElement.clientWidth
+        this.setState({height, width}, () => {
+            console.log(this.state.height, this.state.width)
+        })
 
-
-render() {
-
-  const handleMDown = (e) => {
-	console.log("touched", e, e.target.getBoundingClientRect())
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = e.clientX - currentTargetRect.left
-	let y = e.clientY - currentTargetRect.top
-	x = x/this.state.width
-	y = y/this.state.height
-	this.setState({lastX: x, lastY: y})
-	this.setState({mouseDown: true})
-	ipcRenderer.send('click', {type:14, x:x, y:y})
-}
-  const handleMUp = (e) => {
-	console.log("touched end", e)
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = e.clientX - currentTargetRect.left
-	let y = e.clientY - currentTargetRect.top
-	x = x/this.state.width
-	y = y/this.state.height
-	this.setState({mouseDown: false})
-	ipcRenderer.send('click', {type:16,  x:x, y:y})
-}
+        ipcRenderer.on('plugged', () => {
+            this.setState({status: true})
+        })
+        ipcRenderer.on('unplugged', () => {
+            this.setState({status: false})
+        })
+        ipcRenderer.send('statusReq')
+    }
 
 
-  const handleMMove = (e) => {
-	console.log("touched drag", e)
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = e.clientX - currentTargetRect.left
-	let y = e.clientY - currentTargetRect.top
-	x = x/this.state.width
-	y = y/this.state.height
-	ipcRenderer.send('click', {type:15,  x:x, y:y})
-}
+    render() {
 
-  const handleDown = (e) => {
-	console.log("touched", e, e.target.getBoundingClientRect())
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = e.touches[0].clientX - currentTargetRect.left
-	let y = e.touches[0].clientY - currentTargetRect.top
-	x = x/this.state.width
-	y = y/this.state.height
-	this.setState({lastX: x, lastY: y})
-	this.setState({mouseDown: true})
-	ipcRenderer.send('click', {type:14, x:x, y:y})
-}
-  const handleUp = (e) => {
-	console.log("touched end", e)
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = this.state.lastX
-	let y = this.state.lastY
-	this.setState({mouseDown: false})
-	ipcRenderer.send('click', {type:16,  x:x, y:y})
-}
+        const handleMDown = (e) => {
+            console.log("touched", e, e.target.getBoundingClientRect())
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = e.clientX - currentTargetRect.left
+            let y = e.clientY - currentTargetRect.top
+            x = x / this.state.width
+            y = y / this.state.height
+            this.setState({lastX: x, lastY: y})
+            this.setState({mouseDown: true})
+            ipcRenderer.send('click', {type: 14, x: x, y: y})
+        }
+        const handleMUp = (e) => {
+            console.log("touched end", e)
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = e.clientX - currentTargetRect.left
+            let y = e.clientY - currentTargetRect.top
+            x = x / this.state.width
+            y = y / this.state.height
+            this.setState({mouseDown: false})
+            ipcRenderer.send('click', {type: 16, x: x, y: y})
+        }
 
 
-  const handleMove = (e) => {
-	console.log("touched drag", e)
-	let currentTargetRect = e.target.getBoundingClientRect();
-	let x = e.touches[0].clientX - currentTargetRect.left
-	let y = e.touches[0].clientY - currentTargetRect.top
-	x = x/this.state.width
-	y = y/this.state.height
-	ipcRenderer.send('click', {type:15,  x:x, y:y})
-}
+        const handleMMove = (e) => {
+            console.log("touched drag", e)
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = e.clientX - currentTargetRect.left
+            let y = e.clientY - currentTargetRect.top
+            x = x / this.state.width
+            y = y / this.state.height
+            ipcRenderer.send('click', {type: 15, x: x, y: y})
+        }
+
+        const handleDown = (e) => {
+            console.log("touched", e, e.target.getBoundingClientRect())
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = e.touches[0].clientX - currentTargetRect.left
+            let y = e.touches[0].clientY - currentTargetRect.top
+            x = x / this.state.width
+            y = y / this.state.height
+            this.setState({lastX: x, lastY: y})
+            this.setState({mouseDown: true})
+            ipcRenderer.send('click', {type: 14, x: x, y: y})
+        }
+        const handleUp = (e) => {
+            console.log("touched end", e)
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = this.state.lastX
+            let y = this.state.lastY
+            this.setState({mouseDown: false})
+            ipcRenderer.send('click', {type: 16, x: x, y: y})
+        }
 
 
-  return (
-    <div style={{height: '100%'}}>
-    <div ref={(divElement)=>{this.divElement = divElement}}  
-	className="App" 
-	onTouchStart={handleDown}
-	onTouchEnd={handleUp}
-	onTouchMove={(e) => {if(this.state.mouseDown){handleMove(e)}}}
-	onMouseDown={handleMDown}
-	onMouseUp={handleMUp}
-	onMouseMove={(e) => {if(this.state.mouseDown){handleMMove(e)}}}
-	style={{height: '100%', width:'100%', padding: 0, margin: 0}}>
-            <JsmpegPlayer
-                wrapperClassName={"video-wrapper"}
-                videoUrl={"ws://localhost:8082/supersecret"}
-                options={{
-                    autoplay: true
+        const handleMove = (e) => {
+            console.log("touched drag", e)
+            let currentTargetRect = e.target.getBoundingClientRect();
+            let x = e.touches[0].clientX - currentTargetRect.left
+            let y = e.touches[0].clientY - currentTargetRect.top
+            x = x / this.state.width
+            y = y / this.state.height
+            ipcRenderer.send('click', {type: 15, x: x, y: y})
+        }
+
+
+        return (
+            <div style={{height: '100%'}}>
+                <div ref={(divElement) => {
+                    this.divElement = divElement
                 }}
-            ></JsmpegPlayer>
-    </div>
-    </div>
-  );
+                     className="App"
+                     onTouchStart={handleDown}
+                     onTouchEnd={handleUp}
+                     onTouchMove={(e) => {
+                         if (this.state.mouseDown) {
+                             handleMove(e)
+                         }
+                     }}
+                     onMouseDown={handleMDown}
+                     onMouseUp={handleMUp}
+                     onMouseMove={(e) => {
+                         if (this.state.mouseDown) {
+                             handleMMove(e)
+                         }
+                     }}
+                     style={{height: '100%', width: '100%', padding: 0, margin: 0, display: 'flex'}}>
+                    {this.state.status ?
+                        <JsmpegPlayer
+                            wrapperClassName={"video-wrapper"}
+                            videoUrl={"ws://localhost:8082/supersecret"}
+                            options={{
+                                autoplay: true
+                            }}
+                        ></JsmpegPlayer> : <div style={{marginTop: 'auto', marginBottom: 'auto', textAlign: 'center', flexGrow: '1'}}>CONNECT IPHONE TO BEGIN CARPLAY</div>}
+                </div>
+            </div>
+        );
+    }
 }
-}
+
 export default App;
