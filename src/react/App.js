cc5b02040cfec2110792d47b0a647c6a8f8dfcd8
Rhys Morgan <rhys1802@hotmail.co.uk>
2022-03-13 17:44:42 +0000

    first pass jmuxer

--- src/react/App.js
+++ src/react/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import "@fontsource/montserrat";
 import JsmpegPlayer from "./JsmpegPlayer";
+import JMuxer from 'jmuxer';
+
+
 
 const {ipcRenderer} = window;
 
@@ -17,11 +20,27 @@ class App extends Component {
             mouseDown: false,
             lastX: 0,
             lastY: 0,
-            status: false
+            status: false,
+            playing: false,
+            frameCount: 0,
+            fps: 0,
+            start: null,
+            videoDuration: 0
         }
+
     }
 
+
+
     componentDidMount() {
+        const jmuxer = new JMuxer({
+            node: 'player',
+            mode: 'video',
+            //readFpsFromTrack: true,
+            maxDelay: 10,
+            flushingTime: 1,
+            debug: false
+        });
         const height = this.divElement.clientHeight
         const width = this.divElement.clientWidth
         this.setState({height, width}, () => {
@@ -35,12 +54,37 @@ class App extends Component {
             this.setState({status: false})
         })
         ipcRenderer.send('statusReq')
+        const ws = new WebSocket("ws://localhost:3001");
+        ws.binaryType = 'arraybuffer';
+        ws.onmessage = (event) => {
+            //  let duration = 0
+            // if(!(this.state.start)) {
+            //     this.setState({start: new Date().getTime()})
+            // } else {
+            //     let now = new Date().getTime()
+            //     duration = (now - this.state.start)
+            //     this.setState({start: now})
+            // }
+            let buf = Buffer.from(event.data)
+            let duration = buf.readInt32BE(0)
+            let video = buf.slice(4)
+            //console.log("duration was: ", duration)
+            jmuxer.feed({video: new Uint8Array(video), duration:duration})
+            //this.setState({videoDuration: this.state.videoDuration + duration})
+            //console.log(new Date().getTime() - this.state.start, this.state.videoDuration)
+            //this.setState({playing: true})
+        }
     }
+
+
+    /* Now feed media data using feed method. audio and video is buffer data and duration is in milliseconds */
+
 
 
     render() {
 
         const handleMDown = (e) => {
+            e.preventDefault()
             console.log("touched", e, e.target.getBoundingClientRect())
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = e.clientX - currentTargetRect.left
@@ -52,6 +96,7 @@ class App extends Component {
             ipcRenderer.send('click', {type: 14, x: x, y: y})
         }
         const handleMUp = (e) => {
+            e.preventDefault()
             console.log("touched end", e)
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = e.clientX - currentTargetRect.left
@@ -64,6 +109,7 @@ class App extends Component {
 
 
         const handleMMove = (e) => {
+            e.preventDefault()
             console.log("touched drag", e)
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = e.clientX - currentTargetRect.left
@@ -74,6 +120,7 @@ class App extends Component {
         }
 
         const handleDown = (e) => {
+            e.preventDefault()
             console.log("touched", e, e.target.getBoundingClientRect())
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = e.touches[0].clientX - currentTargetRect.left
@@ -85,6 +132,7 @@ class App extends Component {
             ipcRenderer.send('click', {type: 14, x: x, y: y})
         }
         const handleUp = (e) => {
+            e.preventDefault()
             console.log("touched end", e)
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = this.state.lastX
@@ -95,6 +143,7 @@ class App extends Component {
 
 
         const handleMove = (e) => {
+            e.preventDefault()
             console.log("touched drag", e)
             let currentTargetRect = e.target.getBoundingClientRect();
             let x = e.touches[0].clientX - currentTargetRect.left
@@ -107,6 +156,7 @@ class App extends Component {
 
         return (
             <div style={{height: '100%'}}>
+
                 <div ref={(divElement) => {
                     this.divElement = divElement
                 }}
@@ -126,14 +176,10 @@ class App extends Component {
                          }
                      }}
                      style={{height: '100%', width: '100%', padding: 0, margin: 0, display: 'flex'}}>
-                    {this.state.status ?
-                        <JsmpegPlayer
-                            wrapperClassName={"video-wrapper"}
-                            videoUrl={"ws://localhost:8082/supersecret"}
-                            options={{
-                                autoplay: true
-                            }}
-                        ></JsmpegPlayer> : <div style={{marginTop: 'auto', marginBottom: 'auto', textAlign: 'center', flexGrow: '1'}}>CONNECT IPHONE TO BEGIN CARPLAY</div>}
+                    <video  style={{display: this.state.status ? "block" : "none"}} autoPlay onPause={() => console.log("paused")}
+                            id="player"></video>
+                    {this.state.status ? <div></div>
+                         : <div style={{marginTop: 'auto', marginBottom: 'auto', textAlign: 'center', flexGrow: '1'}}>CONNECT IPHONE TO BEGIN CARPLAY</div>}
                 </div>
             </div>
         );
