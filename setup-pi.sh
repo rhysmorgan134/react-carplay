73b905cf3ee002634f2c124c1557e4823ad6d32f
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2023-02-04 18:57:35 +0000

    Update setup-pi.sh

--- setup-pi.sh
+++ setup-pi.sh
@@ -7,7 +7,7 @@ path="/home/pi/Desktop/Carplay.AppImage"
 echo "Creating udev rules"
 
 FILE=/etc/udev/rules.d/52-nodecarplay.rules
-echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"1520\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
+echo "SUBSYSTEM==\"usb\", ATTR{idVendor}==\"1314\", ATTR{idProduct}==\"152*\", MODE=\"0660\", GROUP=\"plugdev\"" | sudo tee $FILE
 
 if [[ $? -eq 0 ]]; then
 	echo -e Permissions created'\n'
