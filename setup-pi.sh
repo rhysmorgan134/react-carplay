92f5b22c768c7531eb9a30c0d021594f17d536a1
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2023-07-31 20:46:06 +0100

    Update setup-pi.sh auto detect 64 or 32, user name agnostic

--- setup-pi.sh
+++ setup-pi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Set path to copy appimage and autolaunch location
-path="/home/pi/Desktop/Carplay.AppImage"
+path="/home/$USER/Desktop/Carplay.AppImage"
 
 #create udev rule thats specific to carlinkit device
 echo "Creating udev rules"
@@ -17,18 +17,23 @@ fi
 
 echo "Downloading AppImage"
 
-curl -L https://github.com/rhysmorgan134/react-carplay/releases/download/v3.0.1/ReactCarplay-3.0.1-armv7l.AppImage --output /home/pi/Desktop/Carplay.AppImage
+if getconf LONG_BIT | grep -q '64'; then
+	echo "64 Bit Detected"
+ 	curl -L https://github.com/rhysmorgan134/react-carplay/releases/download/v3.0.2/ReactCarplay-3.0.2-arm64.AppImage --output /home/$USER/Desktop/Carplay.AppImage
+else
+	echo "32 Bit Detected"
+ 	curl -L https://github.com/rhysmorgan134/react-carplay/releases/download/v3.0.1/ReactCarplay-3.0.1-armv7l.AppImage --output /home/$USER/Desktop/Carplay.AppImage
 
 echo "Download Done"
 
 echo "Creating executable"
-sudo chmod +x /home/pi/Desktop/Carplay.AppImage
+sudo chmod +x /home/$USER/Desktop/Carplay.AppImage
 
 echo "Creating Autostart File"
 
 sudo bash -c "echo '[Desktop Entry]
 Name=File Manager
-Exec=/home/pi/Desktop/Carplay.AppImage
+Exec=/home/$USER/Desktop/Carplay.AppImage
 Type=Application' > /etc/xdg/autostart/carplay.desktop"
 
 echo "All Done"
