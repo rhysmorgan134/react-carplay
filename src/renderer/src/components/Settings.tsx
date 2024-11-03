ff6ddb9954e7052a0584c0a8440a8538770d344d
rhysmorgan134 <36861722+rhysmorgan134@users.noreply.github.com>
2024-11-03 10:15:34 +0000

    id fix and piMost removal

--- src/renderer/src/components/Settings.tsx
+++ src/renderer/src/components/Settings.tsx
@@ -93,6 +93,23 @@ function Settings({ settings }: SettingsProps) {
         </Grid>
       )
     },
+    piMost: () => {
+      return (
+        <Grid key={'pimost'} xs={4}>
+          <FormControl>
+            <FormControlLabel id={'pimost'}  label={'PIMOST'} control={<Checkbox disabled={true} checked={false} onChange={(_: React.ChangeEvent<HTMLInputElement>) => {
+              // settingsChange('piMost', event.target.checked)
+              if(activeSettings.piMost) {
+                settingsChange('piMost', false)
+                settingsChange('most', {})
+              } else {
+                setOpenStream(true)
+              }
+            }}/>} />
+          </FormControl>
+        </Grid>
+      )
+    },
     nightMode: () => {
       return (
         <Grid key={'nightMode'} xs={4}>
